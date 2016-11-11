3443ce5c4157416f6bde80cc0ce93245743811dc
Sally Wieland <sallybwieland@gmail.com>
2016-11-11 15:15:40 -0500

    follow / unfollow fixed and working

--- components/OtherUsers.js
+++ components/OtherUsers.js
@@ -11,27 +11,59 @@ class OtherUsers extends React.Component {
     }
     componentDidMount() {
         // fetching the other users to display.
-        fetch('https://still-springs-37963.herokuapp.com/not_followed?api_token=' + sessionStorage.getItem('api_token'))
+
+        fetch('https://still-springs-37963.herokuapp.com/followed?api_token=' + sessionStorage.getItem('api_token'))
         .then(response => response.json())
         // .then(response => console.log(response))
-        .then((response) => {
-            this.setState({
-                users: response.users
-                // setting state for the users so the correct ones show for the matching API token (whoever's logged in)
+        .then((following) => {
+            following.users = following.users.map((user) => {
+                user.following = true
+                return user
+            })
+
+            fetch('https://still-springs-37963.herokuapp.com/not_followed?api_token=' + sessionStorage.getItem('api_token'))
+            .then(response => response.json())
+            // .then(response => console.log(response))
+            .then((unfollowing) => {
+                var users = following.users.concat(unfollowing.users)
+                users.sort()
+                this.setState({
+                    users: users
+                    // setting state for the users so the correct ones show for the matching API token (whoever's logged in)
+                })
             })
         })
     }
 
-    follow(id) {
-        fetch('https://still-springs-37963.herokuapp.com/followed', {
+    follow(id, userIndex) {
+        fetch('https://still-springs-37963.herokuapp.com/users/' + id + '/follow', {
             body: JSON.stringify({
                 id: id,
                 api_token: sessionStorage.getItem('api_token') // have use body for push
                 // bringing in the API token to show whoever's logged in followers list.
             }),
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
         })
         .then(response => response.json())
+        .then(response => {
+            var users = this.state.users // makes copy of users
+            // users[userIndex].following = ! users[userIndex].following // target specific users you are interacting with
+            if (users[userIndex].following !== true) {
+                // not equal to true --> set it to true
+                users[userIndex].following = true
+            }
+            else {
+                users[userIndex].following = false
+                // if it is equal to true --> set it to false (toggle)
+            }
+            this.setState({
+                users: users
+            })
+        })
+
     }
 
     render() {
@@ -42,7 +74,7 @@ class OtherUsers extends React.Component {
                 <div className="row individual_users">
                     {user.firstname} {user.lastname}<br />
                     <span className="username">@{user.username}</span>
-                    <button className="btn follow_button" type="button" onClick={() => this.follow(user.id)}><span className="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
+                    <button className="btn follow_button unfollow_button" type="button" onClick={() => this.follow(user.id, i)}><span className={user.following?"glyphicon glyphicon-minus":"glyphicon glyphicon-plus"} aria-hidden="true"></span></button>
                 </div>
 
             </div>
